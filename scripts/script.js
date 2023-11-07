14b2b7b7a8c51d9e118bef8f6585cd74c17720fa
AlexPonomarevS <asponomarev1@yandex.ru>
2023-11-07 12:48:41 +0300

    fix lab-4!

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 (function() {
     window.addEventListener('load', function() {
-        var loadTime = performance.timing.domContentLoadedEventEnd - performance.timing.navigationStart;
+        var loadTime = performance.timing.domContentLoadedEventEnd - performance.timing.domContentLoadedEventStart;
         document.getElementById('loadTime').textContent = 'Страница загружена за ' + loadTime + ' мс';
     });
 })();
