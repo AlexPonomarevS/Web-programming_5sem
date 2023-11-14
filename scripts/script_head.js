7ecb133f9151b9fab10334c3b6276be33150eb07
AlexPonomarevS <asponomarev1@yandex.ru>
2023-11-14 19:58:44 +0300

    fix lab-4!

---
+++ scripts/script_head.js
@@ -0,0 +1,7 @@
+const currentUrl = document.location.href;
+const menuItems = document.querySelectorAll('.header__menu__ul__elem-a');
+menuItems.forEach(item => {
+    if (item.getAttribute('href') === currentUrl) {
+        item.classList.add('active');
+    }
+});
