0c5a450cdcd1ac00234c4ff7dcd5870f2b2e847b
AlexPonomarevS <asponomarev1@yandex.ru>
2023-11-21 13:09:33 +0300

    labs fix

--- scripts/script_head.js
+++ scripts/script_head.js
@@ -1,7 +1,9 @@
-const currentUrl = document.location.href;
-const menuItems = document.querySelectorAll('.header__menu__ul__elem-a');
-menuItems.forEach(item => {
-    if (item.getAttribute('href') === currentUrl) {
-        item.classList.add('active');
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    const currentUrl = document.location.href;
+    const menuItems = document.querySelectorAll('.header__menu__ul__elem-a');
+    menuItems.forEach(item => {
+        if (item.getAttribute('href') === currentUrl) {
+            item.classList.add('active');
+        }
+    });
 });
